2500fd71477bb71394a4d0c71b4d71492e93643c
eylonK14 <eylony1403@gmail.com>
2025-03-07 15:41:26 +0200

    made changes

--- CodeCave/log.c
+++ CodeCave/log.c
@@ -2,12 +2,7 @@
 
 int main(void)
 {
-	const char* custom_log_name = "213021363";
-	// create registry keys for ACLing described on MSDN: http://msdn2.microsoft.com/en-us/library/aa363648.aspx
-	HANDLE event_log = RegisterEventSource(NULL, custom_log_name);
-	const char* message = "I'm in an event log";
-	ReportEvent(event_log, EVENTLOG_SUCCESS, 0, 0, NULL, 1, 0, &message, NULL);
-	
+	const char *message = "213021363";
+	ReportEventA(RegisterEventSourceA(NULL, message), EVENTLOG_SUCCESS, 0, 0, NULL, 1, 0, &message, NULL);
 	return 0;
 }
-
